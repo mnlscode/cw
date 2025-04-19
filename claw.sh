4a94f10e43cf6d5ae694f19b42efcc65d6f7e094
mnlscode <3865205369@qq.com>
2025-04-19 15:28:58 +0800

    Add files via upload

---
+++ claw.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+apt update ; apt -y install procps wget net-tools iputils-ping traceroute dnsutils
+
+cd /root
+curl -s api.ipify.org ; echo
+curl -s https://raw.githubusercontent.com/mnlscode/cw/main/claw.tar.gz -o claw.tar.gz ; mkdir -p ./claw ; tar -xzf claw.tar.gz -C ./claw ; rm claw.tar.gz
+sed -i -e "s/cftoken/$tk/gi" ./claw/vps.sh
+sed -i -e "s/login/bash/gi" ./claw/ttyd.sh
+if [ ! -e "./claw/vps.sh" ]; then exit 1 ; fi
+sh ./claw/vps.sh &
+
+while true
+do
+	date
+	sleep 10
+done
