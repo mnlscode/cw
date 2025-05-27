bf9ebe7b65ab3da2b0fa9ec10130e1229999182a
mnlscode <3865205369@qq.com>
2025-05-27 22:31:51 +0800

    Update claw2.sh

--- claw2.sh
+++ claw2.sh
@@ -2,7 +2,7 @@
 #apt update ; apt -y install procps wget net-tools iputils-ping traceroute dnsutils
 apt update ; apt -y install procps
 
-ulimit -v 131072
+ulimit -v 153600
 ulimit -a
 
 cd /root
@@ -17,6 +17,6 @@ sh ./claw/vps.sh &
 
 while true
 do
-	ps aux ; echo 009====================================================
+	ps aux ; echo 010====================================================
 	sleep 5
 done
