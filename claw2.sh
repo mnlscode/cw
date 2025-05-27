88dc8327123dd7da504d09ec4e5dc41db66e4fd6
mnlscode <3865205369@qq.com>
2025-05-27 20:25:07 +0800

    Update claw2.sh

--- claw2.sh
+++ claw2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-apt update ; apt -y install procps wget net-tools iputils-ping traceroute dnsutils
+#apt update ; apt -y install procps wget net-tools iputils-ping traceroute dnsutils
+apt update ; apt -y install procps wget
 
 cd /root
 curl -s api.ipify.org ; echo
